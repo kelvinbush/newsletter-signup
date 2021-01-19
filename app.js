13e9091301f875d01bb0b5eb28ffa7ce31c7dce8
Kelvin Bush <kelybush@gmail.com>
2021-01-19 12:36:45 +0300

    adding mailchimp

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.post("/", (req, res) => {
 
     const jsonData = JSON.stringify(data);
 
-    const url = "https://us7.api.mailchimp.com/3.0/lists/b08a760e38"
+    const url = "https://us7.api.mailchimp.com/3.0/ists/b08a760e38"
     const options = {
         method: "POST",
         auth: "kelvin:5923a40e22d197bf4d0aa65e0e305560-us7"
@@ -40,8 +40,8 @@ app.post("/", (req, res) => {
 
     const request = https.request(url, options, (response) => {
         if (response.statusCode === 200) {
-            res.send("Successfully subscribed");
-        } else res.send("There was an error lease try  again later");
+            res.sendFile(__dirname + "/success.html");
+        } else res.sendFile(__dirname + "/failure.html");
 
         response.on("data", (data) => {
             console.log(JSON.parse(data));
@@ -50,6 +50,10 @@ app.post("/", (req, res) => {
 
     request.write(jsonData);
     request.end();
+})
+
+app.post("/failure.html", (req, res) => {
+    res.redirect("/");
 })
 
 app.listen(3000, () => {

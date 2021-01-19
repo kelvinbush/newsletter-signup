d23dda48b19b787c5d9e589e8b1320cd78b0eb16
Kelvin Bush <kelybush@gmail.com>
2021-01-19 12:47:01 +0300

    polishing

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.post("/failure.html", (req, res) => {
     res.redirect("/");
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("server started on port 3000");
 })
 
